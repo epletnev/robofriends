9a480c4e58f2124f0f7b255ea96bce100381cde3
Rajiv Maluste <Rajiv@rajivserver.local>
2019-11-28 16:21:11 +0000

    Updates

---
+++ precache-manifest.ff45bb54a014e7dc2613175b85cf4c0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea5a32d17a66fe6df2f6aa1deb009178",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "e97070d393ad87044474",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b782cf1bb77caf7d8d4e",
+    "url": "/robofriends/static/css/main.f09cde35.chunk.css"
+  },
+  {
+    "revision": "e97070d393ad87044474",
+    "url": "/robofriends/static/js/2.814c5b91.chunk.js"
+  },
+  {
+    "revision": "b782cf1bb77caf7d8d4e",
+    "url": "/robofriends/static/js/main.ee8416e8.chunk.js"
+  },
+  {
+    "revision": "7b4f849e958ac4107787",
+    "url": "/robofriends/static/js/runtime-main.a1071882.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
